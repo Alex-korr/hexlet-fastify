935bfe1d60d76c56165dcad6bba5948d74b72594
ALEKSEI <alex@ALEKSEIs-MacBook-Pro.local>
2025-10-24 10:53:43 +0300

    Add hello route

--- routes/root.js
+++ routes/root.js
@@ -2,4 +2,23 @@ export default async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     return { root: true }
   })
+
+   fastify.get('/users', async (request, reply) => {
+    return 'GET /users'
+  })
+
+  fastify.post('/users', async (request, reply) => {
+    return 'POST /users'
+  })
+
+  fastify.get('/hello', async (request, reply) => {
+    const { name } = request.query
+
+    if (name) {
+      return `Hello, ${name}!`
+    } else {
+      return 'Hello, World!'
+    }
+  })  
+
 }
